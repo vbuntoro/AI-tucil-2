79c09425c58bec391cd07f4675730eb586e3843b
michaelaw320 <michaelaw320@gmail.com>
2014-11-05 16:02:36 +0700

    done

--- Tucil2/src/tucil2/Tucil2.java
+++ Tucil2/src/tucil2/Tucil2.java
@@ -22,6 +22,8 @@ import java.io.IOException;
 import java.util.Random;
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.functions.MultilayerPerceptron;
 import weka.classifiers.lazy.IBk;
 import weka.classifiers.trees.J48;
 import weka.core.Instances;
@@ -35,7 +37,6 @@ import weka.core.converters.ConverterUtils.DataSource;
  */
 public class Tucil2 {
 
-
     /**
      * @param args the command line arguments
      */
@@ -43,7 +44,7 @@ public class Tucil2 {
         // TODO code application logic here
         Instances dataset = DataSource.read(args[0]);
         dataset.setClassIndex(dataset.numAttributes() - 1);
-        if(args[1].equals("J48")) {
+        if (args[1].equals("J48")) {
             String[] options = new String[4];
             options[0] = "-C";
             options[1] = "0.25";
@@ -55,10 +56,10 @@ public class Tucil2 {
             System.out.println(klas.toSummaryString());
             //Evaluation Build
             Evaluation eval = new Evaluation(dataset);
-            if(args[2].equals("cross")) {
+            if (args[2].equals("cross")) {
                 eval.crossValidateModel(klas, dataset, 10, new Random(1));
-                System.out.println(eval.toSummaryString("\nResults\n\n", false));  
-            } else if(args[2].equals("fullset")) {
+                System.out.println(eval.toSummaryString("\nResults\n\n", false));
+            } else if (args[2].equals("fullset")) {
                 Classifier cls = new J48();
                 cls.buildClassifier(dataset);
                 eval.evaluateModel(cls, dataset);
@@ -67,35 +68,85 @@ public class Tucil2 {
                 System.err.println("args 2 must be either cross or fullset only");
                 System.exit(1);
             }
+        } else if (args[1].equals("ibk")) {
+            String[] options = new String[6];
+            options[0] = "-K";
+            options[1] = "1";
+            options[2] = "-W";
+            options[3] = "0";
+            options[4] = "-A";
+            options[5] = "weka.core.neighboursearch.LinearNNSearch -A \"weka.core.EuclideanDistance -R first-last\"";
+            IBk klas = new IBk();
+            klas.setOptions(options);
+            klas.buildClassifier(dataset);
+            //Evaluation Build
+            Evaluation eval = new Evaluation(dataset);
+            if (args[2].equals("cross")) {
+                eval.crossValidateModel(klas, dataset, 10, new Random(1));
+                System.out.println(eval.toSummaryString("\nResults\n\n", false));
+            } else if (args[2].equals("fullset")) {
+                Classifier cls = new IBk();
+                cls.buildClassifier(dataset);
+                eval.evaluateModel(cls, dataset);
+                System.out.println(eval.toSummaryString("\nResults\n\n", false));
+            } else {
+                System.err.println("args 2 must be either cross or fullset only");
+                System.exit(1);
+            }
+        } else if (args[1].equals("perceptron")) {
+            String[] options = new String[15];
+            options[0] = "-L";
+            options[1] = "0.3";
+            options[2] = "-M";
+            options[4] = "0.2";
+            options[5] = "-N";
+            options[6] = "500";
+            options[7] = "-V";
+            options[8] = "0";
+            options[9] = "-S";
+            options[10] = "0";
+            options[11] = "-E";
+            options[12] = "20";
+            options[13] = "-H";
+            options[14] = "a";
+            MultilayerPerceptron klas = new MultilayerPerceptron();
+            klas.setOptions(options);
+            klas.buildClassifier(dataset);
+            //Evaluation Build
+            Evaluation eval = new Evaluation(dataset);
+            if (args[2].equals("cross")) {
+                eval.crossValidateModel(klas, dataset, 10, new Random(1));
+                System.out.println(eval.toSummaryString("\nResults\n\n", false));
+            } else if (args[2].equals("fullset")) {
+                Classifier cls = new MultilayerPerceptron();
+                cls.buildClassifier(dataset);
+                eval.evaluateModel(cls, dataset);
+                System.out.println(eval.toSummaryString("\nResults\n\n", false));
+            } else {
+                System.err.println("args 2 must be either cross or fullset only");
+                System.exit(1);
+            }
+        } else if (args[1].equals("bayes")) {
+            NaiveBayes klas = new NaiveBayes();
+            klas.buildClassifier(dataset);
+            //Evaluation Build
+            Evaluation eval = new Evaluation(dataset);
+            if (args[2].equals("cross")) {
+                eval.crossValidateModel(klas, dataset, 10, new Random(1));
+                System.out.println(eval.toSummaryString("\nResults\n\n", false));
+            } else if (args[2].equals("fullset")) {
+                Classifier cls = new NaiveBayes();
+                cls.buildClassifier(dataset);
+                eval.evaluateModel(cls, dataset);
+                System.out.println(eval.toSummaryString("\nResults\n\n", false));
+            } else {
+                System.err.println("args 2 must be either cross or fullset only");
+                System.exit(1);
+            }
+        } else {
+            System.err.println("Valid args 1 are J48 / ibk / perceptron / bayes");
+            System.exit(1);
         }
-        else if(args[1].equals("ibk")) {
-                String[] options = new String[6];
-                options[0] = "-K";
-                options[1] = "1";
-                options[2] = "-W";
-                options[3] = "0";
-                options[4] = "-A";
-                options[5] = "weka.core.neighboursearch.LinearNNSearch -A \"weka.core.EuclideanDistance -R first-last\"";            
-                IBk klas = new IBk();
-                klas.setOptions(options);
-                klas.buildClassifier(dataset);
-                //Evaluation Build
-                Evaluation eval = new Evaluation(dataset);
-                if(args[2].equals("cross")) {
-                    eval.crossValidateModel(klas, dataset, 10, new Random(1));
-                    System.out.println(eval.toSummaryString("\nResults\n\n", false));  
-                } else if(args[2].equals("fullset")) {
-                    Classifier cls = new IBk();
-                    cls.buildClassifier(dataset);
-                    eval.evaluateModel(cls, dataset);
-                    System.out.println(eval.toSummaryString("\nResults\n\n", false));
-                } else {
-                    System.err.println("args 2 must be either cross or fullset only");
-                    System.exit(1);
-                }
     }
-        
-    }
-    
-    
+
 }
